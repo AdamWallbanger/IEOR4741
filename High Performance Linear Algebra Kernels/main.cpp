391db97e92050b165f406bbd195891958231cc54
Adam Wang <hw3000@columbia.edu>
2025-09-13 07:10:50 -0400

    3 Test case

--- High Performance Linear Algebra Kernels/main.cpp
+++ High Performance Linear Algebra Kernels/main.cpp
@@ -10,12 +10,14 @@ void multiply_mm_transposed_b(const double* matrixA, int rowsA, int colsA, const
 
 double* create_matrix(int row,int column)
 {
+    int count = 1;
     double* matrix = new double[row*column];
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < column; j++)
         {
-            matrix[i * column + j] = 2;
+            matrix[i * column + j] = count;
+            count++;
         }
     }
     return matrix;
@@ -23,10 +25,12 @@ double* create_matrix(int row,int column)
 
 double* create_vector(int row)
 {
+    int count = 1;
     double* vector = new double[row];
     for (int i = 0; i < row; i++)
     {
-        vector[i] = 2;
+        vector[i] = count;
+        count++;
     }
     return vector;
 }
@@ -47,19 +51,38 @@ class Timer
 
 int main()
 {
-    int row,column;
-    row = 3;
-    column = 2;
-    double* matrix = create_matrix(row,column);
-    double* vector = create_vector(column);
-    double* matrixA = create_matrix(row,column);
-    double* matrixB = create_matrix(row,column);
-    double result[column];
-    delete[] matrix;
-    delete[] vector;
-    Timer timer;
-    long long duration = timer.elapsed_microseconds();
-    cout << duration << endl;
+    int test_dim[3] = {4,64,256};
+
+    for (int i = 0; i < 3; i++)
+    {
+        double result_mv[test_dim[i]];
+        double result_mm[test_dim[i]*test_dim[i]];
+        double* matrix = create_matrix(test_dim[i],test_dim[i]);
+        double* vector = create_vector(test_dim[i]);
+        double* matrixA = create_matrix(test_dim[i],test_dim[i]);
+        double* matrixB = create_matrix(test_dim[i],test_dim[i]);
+        Timer timer;
+        multiply_mv_row_major(matrix,test_dim[i],test_dim[i],vector,result_mv);
+        long long duration = timer.elapsed_microseconds();
+        cout <<test_dim[i] << " Size MV Row_major: " << duration << "ms" <<endl;
+        // Timer timer1;
+        // multiply_mv_col_major(matrix,test_dim[i],test_dim[i],vector,result);
+        // long long duration1 = timer1.elapsed_microseconds();
+        // cout <<test_dim[i] << " Size MV Col_major: " << duration1 << "ms" <<endl;
+        delete[] matrix;
+        delete[] vector;
+        Timer timer2;
+        multiply_mm_naive(matrixA,test_dim[i],test_dim[i],matrixB,test_dim[i],test_dim[i],result_mm);
+        long long duration2 = timer2.elapsed_microseconds();
+        cout <<test_dim[i] << " Size MM Naive: " << duration2 << "ms" <<endl;
+        Timer timer3;
+        multiply_mm_transposed_b(matrixA,test_dim[i],test_dim[i],matrixB,test_dim[i],test_dim[i],result_mm);
+        long long duration3 = timer3.elapsed_microseconds();
+        cout <<test_dim[i] << " Size MV Transposed: " << duration3 << "ms" <<endl;
+        delete[] matrixA;
+        delete[] matrixB;
+    }
+
     return 0;
 }
 
